8c1c1d844849778668be1e7671dcd765a1588953
Tomás <talfonsof@gmail.com>
2025-04-05 00:26:14 -0300

    Correcion - Pestañas principales

--- main.py
+++ main.py
@@ -1,51 +1,59 @@
 import flet as ft
-
-conteiner = ft.Container(
-    ft.Column([
-        # Titulo
-        ft.Container(
-            ft.Text("Iniciar Sesión", width=380, size=30, text_align="center", weight="w900"),
-            ft.padding.only(10, 100, 0, 20)
-        ),
-        # TextField del Usuario
-        ft.Container(
-            ft.TextField(width=350, hint_text="Ingrese su Usuario", color="white", prefix_icon= ft.icons.PERSON),
-            ft.padding.only(20),
-        ),
-        # TextField de la Contraseña
-        ft.Container(
-            ft.TextField(width=350, hint_text="Ingrese su Contraseña", color="white", prefix_icon= ft.icons.LOCK, password=True, can_reveal_password=True),
-            ft.padding.only(20),
-        ),
-        # Button para Iniciar Sesion
-        ft.Container(
-            ft.ElevatedButton(text="Ingresar", width=280, height=40, bgcolor="#212121"),
-            ft.padding.only(60, 30),
-        )
-    ]
-    ),
-    border_radius=20,
-    width=400,
-    height=530,
-    bgcolor="#673AB7"
-)
+from views import principal
 
 def main(page: ft.Page):
+
+    def cam_ventana(route):
+        page.controls.clear()
+        if page.route == "/principal_menu":
+            principal.principal(page)
+        else:
+            page.add(ft.Text("Ruta no encontrada"))
+
+    page.on_route_change = cam_ventana
+    page.go(page.route)
+
     page.window.width = 450
     page.window.height = 600
     page.vertical_alignment = "center"
     page.horizontal_alignment = "center"
     page.title = "StormCargoGest"
-    page.add(conteiner)
 
-    # def cam_ventana(ruta):
-    #     page.controls.clear
-    #     if page.route == "/principal_menu":
-    #         views.principal(page)
-    #     else:
-    #         page.add(conteiner)
-    
-    # page.on_route_change = cam_ventana
-    # page.go(page.route)
-    
+    page.add(
+        ft.Container(
+            ft.Column([
+                # Titulo
+                ft.Container(
+                    ft.Text("Iniciar Sesión", width=380, size=30, text_align="center", weight="w900"),
+                    ft.padding.only(10, 100, 0, 20)
+                ),
+                # TextField del Usuario
+                ft.Container(
+                    ft.TextField(width=350, hint_text="Ingrese su Usuario", color="white", prefix_icon= ft.icons.PERSON),
+                    ft.padding.only(20),
+                ),
+                # TextField de la Contraseña
+                ft.Container(
+                    ft.TextField(width=350, hint_text="Ingrese su Contraseña", color="white", prefix_icon= ft.icons.LOCK, password=True, can_reveal_password=True),
+                    ft.padding.only(20),
+                ),
+                # Button para Iniciar Sesion
+                ft.Container(
+                    ft.ElevatedButton(text="Ingresar", width=280, height=40, bgcolor="#212121", on_click=lambda e: page.go("/principal_menu")),
+                    ft.padding.only(60, 30),
+                ),
+
+                ft.Container(
+                    ft.TextButton(text="Crear Usuario"),
+                    ft.padding.only(150)
+                )
+            ]
+            ),
+            border_radius=20,
+            width=400,
+            height=530,
+            bgcolor="#673AB7"
+        )
+    )
+
 ft.app(target=main)
--- views/principal.py
+++ views/principal.py
@@ -1,59 +1,58 @@
 import flet as ft
 
-conteiner = ft.Container(
-    ft.Column([
-        ft.Container(
-            ft.ElevatedButton(text="Salir", bgcolor="red", height=40),
-            ft.padding.only(1090, 20, 0, 0 )
-        ),
-        ft.Container(
-            ft.Tabs(
-            selected_index=1,
-            animation_duration=300,
-            tabs=[
-                ft.Tab(
-                    text="Pedidos",
-                    icon=ft.Icons.ADD_BOX,
-                    content=ft.Text("This is Tab 3"),
-                ),
-                ft.Tab(
-                    text="Trabajadores",
-                    icon=ft.Icons.PERSON,
-                    content=ft.DataTable(
-                                    columns=[
-                                        ft.DataColumn(ft.Text("Id")),
-                                        ft.DataColumn(ft.Text("Nombre")),
-                                        ft.DataColumn(ft.Text("Rut")),
-                                        ft.DataColumn(ft.Text("Estado"))
-                                    ],
-                                    rows=[
-
-                                    ],
-                    ),
-                ),
-                ft.Tab(
-                    text="Camiones",
-                    icon=ft.Icons.FIRE_TRUCK,
-                    content=ft.Text("This is Tab 3"),
-                ),
-            ],
-            expand=1,
-            ),
-            ft.padding.only(20, 0, 20, 20),
-        )
-    ]),
-    border_radius=20,
-    width=1170,
-    height=740,
-    bgcolor="#673AB7",
-)
-
 def principal_menu(page: ft.Page):
+
+    page.view.append(
+        ft.View(
+            route="/principal_menu",
+            controls=[
+                ft.text(""),
+            ]
+        )
+    )
+
     page.window.width = 1200
     page.window.height = 800
     page.vertical_alignment = "center"
     page.horizontal_alignment = "center"
     page.title = "StormCargoGest"
-    page.add(conteiner)
+
+    page.add(
+        ft.Container(
+            ft.Column([
+                ft.Container(
+                    ft.ElevatedButton(text="Salir", bgcolor="red", height=40),
+                    ft.padding.only(1090, 20, 0, 0 )
+                ),
+                ft.Container(
+                    ft.Tabs(
+                    selected_index=1,
+                    animation_duration=300,
+                    tabs=[
+                        ft.Tab(
+                            text="Pedidos",
+                            icon=ft.Icons.ADD_BOX,
+                            content= ft.TextField(label="Standard")
+                        ),
+                        ft.Tab(
+                            text="Trabajadores",
+                            icon=ft.Icons.PERSON
+                        ),
+                        ft.Tab(
+                            text="Camiones",
+                            icon=ft.Icons.FIRE_TRUCK
+                        ),
+                    ],
+                    expand=1,
+                    ),
+                    ft.padding.only(20, 0, 20, 20),
+                )
+            ]),
+            border_radius=20,
+            width=1170,
+            height=740,
+            bgcolor="#673AB7",
+        )
+    )
 
 ft.app(target=principal_menu)
